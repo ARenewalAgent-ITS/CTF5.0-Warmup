da18c7dec5a1983c008a476b7fa15eb9969577fd
Muhammad Azril <54212814+0xazr@users.noreply.github.com>
2024-01-26 23:06:46 +0700

    Delete intro-to-curl directory

--- intro-to-curl/dist/flag/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(0); 
-if ($_POST["is_admin"] === "true") {
-    echo "ARA5{REDACTED}";
-} else {
-    echo "You are not admin!";
-}
-?>
--- intro-to-curl/src/flag/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(0); 
-if ($_POST["is_admin"] === "true") {
-    echo "ARA5{aw3s0me!_h0w_d1d_y0u_do_th4t??}";
-} else {
-    echo "You are not admin!";
-}
-?>
